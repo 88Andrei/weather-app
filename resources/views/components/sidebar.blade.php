1369d6675ee443ac8439bd3553253a4c8474d8eb
Andrii <freeplay@ukr.net>
2024-11-26 23:08:54 +0100

    Added location routs. + Locations link to the sidebar. Corrected trigger view because of location.

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -19,6 +19,16 @@
             </a>
           </li>
           <li>
+            <a href="{{ route('locations.index') }}" class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+              </svg>
+              
+              <span class="ml-3">Locations</span>
+            </a>
+          </li>
+          <li>
             <a href="{{route('triggers.messages')}}" class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
               <svg
                 aria-hidden="true"
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\WeatherByLocationController;
 use App\Http\Controllers\AirController;
+use App\Http\Controllers\LocationController;
 use App\Http\Controllers\WeatherTriggerController;
 use App\Services\WeatherService;
 
@@ -22,7 +23,6 @@ Route::get('/', [MainController::class , 'index'])->name('home');
 //delet!
 Route::get('/1', [WeatherService::class , 'checkTriggers'])->name('1');
 
-
 Route::post('/location', [WeatherByLocationController::class , 'getWeatherByLocation'])->name('location');
 
 Route::get('/air', [AirController::class , 'index'])->name('air');
@@ -33,6 +33,7 @@ Route::post('/air-in-cities', [AirController::class , 'getHistAirDataOfCities'])
 Route::middleware(['auth'])->group(function () {
     Route::prefix('/dashboard')->group(function(){
         Route::resource('triggers' , WeatherTriggerController::class)->except(['show']);
+        Route::resource('locations' , LocationController::class)->except(['show', 'update', 'edit']);
 
         Route::get('/messages', [WeatherTriggerController::class , 'showMessages'])->name('triggers.messages');
     });
