7129a7425271b668bcbba9bf3dec76c4d9b1e734
Andrii <freeplay@ukr.net>
2025-03-07 22:10:34 +0100

    Added DeleteOldNotification command.

---
+++ app/Console/Commands/DeleteOldNotification.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Notifications\Notification;
+
+class DeleteOldNotification extends Command
+{
+    protected $signature = 'oldnotification:delete';
+
+    protected $description = 'Delete all old weatherTrigger notification';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle(Notification $notifications)
+    {
+        $notifications = DatabaseNotification::where('type', 'App\Notifications\WeatherAlert')->get();
+        $notifications->markAsRead();
+
+        $this->info('Weather trigger delete successfully!');
+    }
+}
